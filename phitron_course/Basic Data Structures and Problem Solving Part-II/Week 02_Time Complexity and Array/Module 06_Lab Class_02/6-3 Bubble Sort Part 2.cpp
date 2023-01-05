4a8b7fbf35c09ff4a4fdf12a57c0bba38a171e94
ImAlAmin <alamin.cse97@gmail.com>
2023-01-05 12:29:58 +0600

    added bubble sort implemention

--- phitron_course/Basic Data Structures and Problem Solving Part-II/Week 02_Time Complexity and Array/Module 06_Lab Class_02/6-3 Bubble Sort Part 2.cpp
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 02_Time Complexity and Array/Module 06_Lab Class_02/6-3 Bubble Sort Part 2.cpp
@@ -1,1 +1,57 @@
+/*
+7
+3 5 1 4 2 6 1
+pass = 0, cnt = n-1
+pass = 1, cnt = n-2
+.
+.
+pass = n-1, cnt = 0
 
+sum = ((n-1) * n) / 2
+    = 0(n^2)
+((n-1)*n) / 2
+n*n
+
+Time Complexity:- 0(n^2)
+Memory Complexity:- 0(n)
+*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    vector<int>a(n);
+    for(int i=0; i<n; i++)
+    {
+        cin >> a[i];
+    }
+    // n ta pass
+    for(int pass = 0; pass < n; pass++)
+    {
+        int last = n-1-pass;
+        bool sorted = true;
+        for(int j=0; j<=last-1; j++)
+        {
+            if(a[j] > a[j+1])
+            {
+                swap(a[j], a[j+1]);
+                sorted = false;
+            }
+
+        }
+        if(sorted)
+            break;
+        cout << "After pass " << pass << ": ";
+        for(int i=0; i<n; i++)
+            cout << a[i] << " ";
+        cout << "\n";
+    }
+    cout << "After sorting: ";
+    for(int i=0; i<n; i++)
+        cout << a[i] << " ";
+    cout << "\n";
+    return 0;
+}
