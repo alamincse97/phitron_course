4e9fe92cdf9688a7edc5fdcb088e128375c488e9
ImAlAmin <alamin.cse97@gmail.com>
2023-01-08 12:58:04 +0600

    added quick sort

---
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 3_Sorting and Introduction to Linked List/Module 08_Merge Sort/8-5 Quick Sort Theory.cpp
@@ -0,0 +1,10 @@
+/*
+1. Quick Sort Theory
+2. Pivot Element
+3. Divide and Conqure
+4. Divide -> 1. Pivot Choose
+          -> 2. All element <= pivot
+          -> 3. All element >= pivot
+          -> 4. total steap -> O(n)
+5. Conqure -> sorted array -> pivot -> sorted array
+*/
