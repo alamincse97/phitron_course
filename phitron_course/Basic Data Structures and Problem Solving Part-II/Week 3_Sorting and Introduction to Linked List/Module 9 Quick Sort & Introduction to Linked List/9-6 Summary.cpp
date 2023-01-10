734205413dd09e5afd9cbf1e4f14dc773b32bbfd
ImAlAmin <alamin.cse97@gmail.com>
2023-01-10 12:10:18 +0600

    added six

---
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 3_Sorting and Introduction to Linked List/Module 9 Quick Sort & Introduction to Linked List/9-6 Summary.cpp
@@ -0,0 +1,11 @@
+/*
+1. Quick Sort Implemention
+2. Quick Sort Complexity
+3. Link List কি ভাবে থাকে।
+4. Array -> sequentaly thake
+5. Linked List -> Arbitaly vabe thake memory jekono jaigai thake
+6. linklist insertion ki vabe thake
+7. linklist index access O(n) thake
+8. Linkist e amra alows Linear Search hobe
+8. Linkist e amra alows Linear Search hobe
+*/
