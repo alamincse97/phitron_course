403112263c462907ff6a2b5953dd701a873fcc65
ImAlAmin <alamin.cse97@gmail.com>
2023-01-10 11:42:55 +0600

    added link list 2

---
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 3_Sorting and Introduction to Linked List/Module 9 Quick Sort & Introduction to Linked List/9-4 Intro to Linked List 2.cpp
@@ -0,0 +1,9 @@
+/*
+1. Linear data structure
+2. Dynamic data structure
+3. Linked List দিয়া আমরা বানাতে পারি।
+link list er use case
+1. memory valo moto utilize korte pare
+2. onek bro size er array niye kaj korte hole amara linklist use korte pari
+3. kisu data structure banate stack, queue, dequeue lage
+*/
