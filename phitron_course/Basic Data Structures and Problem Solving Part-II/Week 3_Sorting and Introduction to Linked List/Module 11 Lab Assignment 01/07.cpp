222cf46d47f169130664e26c8b4aaab4e43a1830
ImAlAmin <alamin.cse97@gmail.com>
2023-01-16 20:33:28 +0600

    assinment 07 done

--- phitron_course/Basic Data Structures and Problem Solving Part-II/Week 3_Sorting and Introduction to Linked List/Module 11 Lab Assignment 01/07.cpp
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 3_Sorting and Introduction to Linked List/Module 11 Lab Assignment 01/07.cpp
@@ -1,1 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+    int n;
+    cin >> n;
+    int arr[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> arr[i];
+    }
+    int a, b;
+    cin >> a >> b;
+    for (int i = 0; i < n; i++)
+    {
+        if (i < a - 1 || i > b - 1)
+        {
+            cout << arr[i] << " ";
+        }
+    }
+    cout << endl;
+    return 0;
+}
 
