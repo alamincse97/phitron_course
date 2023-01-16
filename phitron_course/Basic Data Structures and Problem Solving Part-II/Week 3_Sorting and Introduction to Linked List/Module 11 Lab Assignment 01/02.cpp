916574f6f70b0579f658c4eb02ae749a46d4518c
ImAlAmin <alamin.cse97@gmail.com>
2023-01-16 12:45:41 +0600

    assinment 02 done

--- phitron_course/Basic Data Structures and Problem Solving Part-II/Week 3_Sorting and Introduction to Linked List/Module 11 Lab Assignment 01/02.cpp
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 3_Sorting and Introduction to Linked List/Module 11 Lab Assignment 01/02.cpp
@@ -1,1 +1,20 @@
+#include<iostream>
+using namespace std;
+
+int missing_number(int arr[], int n) {
+    int total = (n + 1) * (n + 2) / 2;
+    for (int i = 0; i < n; i++)
+        total -= arr[i];
+    return total;
+}
+
+int main() {
+    int n;
+    cin >> n;
+    int arr[n - 1];
+    for (int i = 0; i < n - 1; i++)
+        cin >> arr[i];
+    cout << missing_number(arr, n - 1) << endl;
+    return 0;
+}
 
