95fa85b5474ea6cab455c96d3ae867be4e9467ef
ImAlAmin <alamin.cse97@gmail.com>
2023-01-17 17:34:32 +0600

    all done

---
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 04_Deep Dive into Linked List/Module 13_More About Linked List/13-4 Intro to Doubly Linked List.cpp
@@ -0,0 +1,7 @@
+/*
+1. we learn Doubly Linked List
+2. Backend movement
+3. Implemention easier
+4. real life implemention
+5. Browser Navigation
+*/
---
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 04_Deep Dive into Linked List/Module 13_More About Linked List/13-7 Summary.cpp
@@ -0,0 +1,9 @@
+/*
+1. singly linked list
+2. insert after value
+3. reverse print of singly linked list
+4. Doubly linked list
+5. structure
+6. insert
+7. Delete
+*/
