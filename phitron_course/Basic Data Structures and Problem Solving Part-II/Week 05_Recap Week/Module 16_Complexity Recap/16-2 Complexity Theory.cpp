816ba139072fe5fbd8946c6cde1b449e1518d5aa
ImAlAmin <alamin.cse97@gmail.com>
2023-01-22 12:42:02 +0600

    time complexity theory

---
+++ phitron_course/Basic Data Structures and Problem Solving Part-II/Week 05_Recap Week/Module 16_Complexity Recap/16-2 Complexity Theory.cpp
@@ -0,0 +1,15 @@
+/*
+ Time and Space Complexity
+ -> Judge time and space
+ -> Rule of tham 10^9 -> 1s
+ -> Polynomial
+ -> O(1) -> Constant time
+ -> O(n) -> Linear time
+ -> O(nlogn)
+ -> O(logn)
+ -> O(root n)
+ -> O(n^2)
+ -> O(n^3)
+ -> O(n^k)
+ -> Exponial complexity :- O(c^n)
+*/
