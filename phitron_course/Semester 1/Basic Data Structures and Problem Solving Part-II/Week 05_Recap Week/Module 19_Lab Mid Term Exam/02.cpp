4e2b3d539d625441ed46acdcafeb5e00039c26e9
ImAlAmin <alamin.cse97@gmail.com>
2023-01-28 17:40:50 +0600

    Assinment 02 Done

---
+++ phitron_course/Semester 1/Basic Data Structures and Problem Solving Part-II/Week 05_Recap Week/Module 19_Lab Mid Term Exam/02.cpp
@@ -0,0 +1,74 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+void merge(int a[], int low, int mid, int high)
+{
+    int n1 = mid - low + 1;
+    int n2 = high - mid;
+    int left[n1], right[n2];
+    for (int i = 0; i < n1; i++)
+    {
+        left[i] = a[low + i];
+    }
+    for (int i = 0; i < n2; i++)
+    {
+        right[i] = a[mid + 1 + i];
+    }
+    int i = 0, j = 0, k = low;
+    while (i < n1 && j < n2)
+    {
+        if (left[i] <= right[j])
+        {
+            a[k++] = left[i++];
+        }
+        else
+        {
+            a[k++] = right[j++];
+        }
+    }
+    while (i < n1)
+    {
+        a[k++] = left[i++];
+    }
+    while (j < n2)
+    {
+        a[k++] = right[j++];
+    }
+}
+
+void mergeSort(int a[], int low, int high)
+{
+    if (low < high)
+    {
+        int mid = (low + high) / 2;
+        mergeSort(a, low, mid);
+        mergeSort(a, mid + 1, high);
+        merge(a, low, mid, high);
+    }
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    int a[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin >> a[i];
+    }
+    mergeSort(a, 0, n - 1);
+    int j = 0;
+    for (int i = 0; i < n; i++)
+    {
+        if (a[i] != a[i + 1])
+        {
+            a[j++] = a[i];
+        }
+    }
+    for (int i = 0; i < j; i++)
+    {
+        cout << a[i] << " ";
+    }
+    return 0;
+}
