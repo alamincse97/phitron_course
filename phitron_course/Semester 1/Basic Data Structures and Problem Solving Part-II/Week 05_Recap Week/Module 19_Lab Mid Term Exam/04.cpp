4d5437d59499951038cf86fba21d4e379064d74a
ImAlAmin <alamin.cse97@gmail.com>
2023-01-28 17:42:20 +0600

    Assinment 04 Done

---
+++ phitron_course/Semester 1/Basic Data Structures and Problem Solving Part-II/Week 05_Recap Week/Module 19_Lab Mid Term Exam/04.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+bool isPalindrome(string s, int start, int end)
+{
+    if (start >= end)
+        return true;
+
+    if (s[start] != s[end])
+        return false;
+
+    return isPalindrome(s, start + 1, end - 1);
+}
+
+int main()
+{
+    string s;
+    cin >> s;
+
+    if (isPalindrome(s, 0, s.length() - 1))
+
+        cout << "Yes" << endl;
+    else
+        cout << "No" << endl;
+
+    return 0;
+}
+
