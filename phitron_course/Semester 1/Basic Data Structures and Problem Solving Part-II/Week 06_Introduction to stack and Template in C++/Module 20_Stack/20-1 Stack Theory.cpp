ccc0fb192c490e2940722dd82cbd8436a57af750
ImAlAmin <alamin.cse97@gmail.com>
2023-01-29 08:55:19 +0600

    stack theory

---
+++ phitron_course/Semester 1/Basic Data Structures and Problem Solving Part-II/Week 06_Introduction to stack and Template in C++/Module 20_Stack/20-1 Stack Theory.cpp
@@ -0,0 +1,13 @@
+/*
+Stack Basic Operation
+1. Top Element Remove
+2. Add a new Top Element
+3. view top element
+4. Array theke ei ta better howar karon stack sob operation O(1) korte pare.
+5. vitorer element dekhte gele depent kore
+6. If array -> then you can
+7. if linked list -> then you can't
+8. stack -> they you can't
+9. real life problem solve in stack
+10. calculator:- expretion evoaloute kora (1+2)*3 + 6/7 + (2*3)
+*/
