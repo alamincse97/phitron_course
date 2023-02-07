90254a90dd33df30d525c9e73b07be65fcb3c47f
ImAlAmin <alamin.cse97@gmail.com>
2023-02-07 22:40:24 +0600

    update

---
+++ phitron_course/Semester 1/Basic Data Structures and Problem Solving Part-II/Week 07_Introduction to Queue/Module 24_Queue/24-2 Queue using Array Theory.cpp
@@ -0,0 +1,20 @@
+/*
+Queue Operation:-
+1. front
+2. enqueue -> element push
+3. dequeue -> front element remove
+4. size ->
+complexity:- O(1)
+implement to static array:-
+a[10] -> queue
+l, r-> queue start and end
+a->[0,0,0,0,0,0,0,0,0,0]
+enqueue 5
+q->[5,0,0,...0]
+enqueue 7
+q->[5,7,0,0..0]
+dequeue 5
+q->[7,8]
+size()
+r-l+1
+*/
