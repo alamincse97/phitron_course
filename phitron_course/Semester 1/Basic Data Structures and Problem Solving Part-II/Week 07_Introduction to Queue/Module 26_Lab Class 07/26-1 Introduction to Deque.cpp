ecc4e06a301543e650e193a6bbf0ee719a38e145
ImAlAmin <alamin.cse97@gmail.com>
2023-02-09 09:38:42 +0600

    deque

---
+++ phitron_course/Semester 1/Basic Data Structures and Problem Solving Part-II/Week 07_Introduction to Queue/Module 26_Lab Class 07/26-1 Introduction to Deque.cpp
@@ -0,0 +1,18 @@
+/*
+Deque -> Double ended queue
+Deque Operation:-
+[1,2,3,4]
+1.push back
+[1,2,3,4,5]
+2.push front
+[6,1,2,3,4,5]
+3.pop back
+[6,1,2,3,4]
+4.pop front
+[1,2,3,4]
+
+Complexity:- O(1)
+
+stack -> pushback + popback
+queue -> pushback + popfront
+*/
