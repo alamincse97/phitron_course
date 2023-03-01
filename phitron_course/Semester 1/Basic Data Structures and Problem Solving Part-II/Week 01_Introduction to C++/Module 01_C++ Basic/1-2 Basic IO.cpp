f0544fc8e4b41d6cb04c2a7786b88b3095bd68c5
ImAlAmin <alamin.cse97@gmail.com>
2023-03-01 18:56:53 +0600

    i/o in c++

---
+++ phitron_course/Semester 1/Basic Data Structures and Problem Solving Part-II/Week 01_Introduction to C++/Module 01_C++ Basic/1-2 Basic IO.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    /*
+    int x, y;
+
+    scanf("%d", &x);
+    cin >> x;
+
+    multiple input
+
+    cin >> x >> y;
+
+    extraction operator ( >> )
+    insertion operator ( << )
+
+    cout << x;
+
+
+    10 11
+    10 11
+
+    cout << x << " " << y << "\n";
+
+    int x, y;
+    char ch;
+    float f;
+    double d;
+
+    cin >> ch >> f >> d;
+
+    cout << ch << " " << f << " " << d << "\n";
+
+    string s;
+    cin >> s;
+
+    cout << s << "\n";
+
+   string concatenation
+
+   string s = "I Love Bangladesh\n";
+   string s2 = "too much";
+
+   s = s+s2;
+   cout << s << "\n";
+   */
+
+   return 0;
+}
