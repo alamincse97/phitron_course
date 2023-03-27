39b76d9008b3351123531930611c3a34e0130d46
ImAlAmin <alamin.cse97@gmail.com>
2023-03-27 15:32:36 +0600

    Dijkstra pseudocode and complexity

---
+++ phitron_course/Semester 2/Introduction to Algorithms & Problem Solving Part - III/Week 04_Path Printing & Dijkstra/Module 13_Dijkstra/13-3 Dijkstra Pseudocode & Complexity.cpp
@@ -0,0 +1,24 @@
+- input -> a weighted graph and a source
+- output -> distance of all nodes from the source
+
+u -> v
+
+if d[u] + c(u, v) < d[v];
+    d[v] = d[u] + c(u, v)
+
+// space complexity -> O(n)
+// time complexity -> O(n) + O(n^2) + O(m) -> O(n^2) + O(n^2) -> O(n^2)
+
+- create a distance array "d"  // O(n)
+- initialize all values of "d" to infinity
+- d[src] = 0
+- create a visited array and mark all nodes as unvisited // O(n)
+
+- for i=0; to n - 1; // O(1)
+    - pick the "unvisited" node with minimum d[node]
+    - visited[node] = 1
+    - for all adj_node of node;
+        - if d[node] + c(node, adj_node) < d[adj_node];
+            - d[adj_node] = d[node] + c(node, adj_node)
+
+- output array "d"
