ae1b8f037da1f6c359a8214c2c18e09bcf71455d
ImAlAmin <alamin.cse97@gmail.com>
2023-05-01 13:28:49 +0600

    sub set sum code

--- phitron_course/Semester 2/Introduction to Algorithms & Problem Solving Part - III/Week 08_Knapsack/Module 24_Knapsack/24_6_Coding with Tabulation.cpp
+++ phitron_course/Semester 2/Introduction to Algorithms & Problem Solving Part - III/Week 08_Knapsack/Module 24_Knapsack/24_6_Coding with Tabulation.cpp
@@ -17,33 +17,40 @@ const int M = 1e5 + 5;
 long long dp[N][M];
 int wt[N], val[N];
 
-int main() {
+int main()
+{
     int n, W;
     cin >> n >> W;
 
-    for(int i = 1 ; i <= n ; i++) {
+    for(int i = 1 ; i <= n ; i++)
+    {
         cin >> wt[i] >> val[i];
     }
 
     // 1. handle base case
-    for(int i = 0 ; i <= W ; i++) {
+    for(int i = 0 ; i <= W ; i++)
+    {
         dp[0][i] = 0;
     }
 
     // 2. loop over the states
-    for(int i = 1 ; i <= n ; i++) {
-        for(int cap = 0 ; cap <= W ; cap++) {
+    for(int i = 1 ; i <= n ; i++)
+    {
+        for(int cap = 0 ; cap <= W ; cap++)
+        {
 
             //2.1. calculate the result from smaller sub-problems
 
-            if(cap < wt[i]) {
+            if(cap < wt[i])
+            {
                 dp[i][cap] = dp[i-1][cap];
             }
-            else {
+            else
+            {
                 dp[i][cap] = max(
-                    val[i] + dp[i - 1][cap - wt[i]],
-                    dp[i - 1][cap]
-                );
+                                 val[i] + dp[i - 1][cap - wt[i]],
+                                 dp[i - 1][cap]
+                             );
             }
         }
     }
