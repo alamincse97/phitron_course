4e5e37cad8e8452ffa2b775a84e29c4f0b2271c5
ImAlAmin <alamin.cse97@gmail.com>
2023-05-10 12:02:09 +0600

    added to tabulation

--- phitron_course/Semester 2/Introduction to Algorithms & Problem Solving Part - III/Week 09_DP Again/Module_30_Lab Class 08/30-2_LCS Memoization.cpp
+++ phitron_course/Semester 2/Introduction to Algorithms & Problem Solving Part - III/Week 09_DP Again/Module_30_Lab Class 08/30-2_LCS Memoization.cpp
@@ -11,4 +11,12 @@ LCS(i, j){
     else:
         return max(LCS(i+1, j), LCS(i, j+1))
 }
+
+1. LCS(i, j) -> Length of the longest common subsequence between strings s[i..n] and t[j..m]
+
+2. LCS(i, j) = 1 + LCS(i+1, j+1) if s[s] == t[j]
+                else max{LCS(i+1, j), LCS(i, j+1)}
+
+3. LCS(i, m) = 0
+   LCS(n, j) = 0
 */
