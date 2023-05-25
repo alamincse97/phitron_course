809228790206db147e7b16c4425d8467f69276a7
ImAlAmin <alamin.cse97@gmail.com>
2023-05-25 17:40:12 +0600

    3.3 Set and set methods in python

---
+++ phitron_course/Semester 3/Week 01_Introduction to Python/Module 03_Basic Python 02/set.py
@@ -0,0 +1,26 @@
+# list --> []
+# tuple --> ()
+# set --> {}
+# set: unique items collection. No duplicate
+numbers = [12, 56, 98, 78, 56, 12, 6, 98]
+print(numbers)
+numbers_set = set(numbers)
+print(numbers_set)
+numbers_set.add(55)
+print(numbers_set)
+numbers_set.remove(6)
+print(numbers_set)
+# print(numbers_set[1] = 5)
+
+for item in numbers_set:
+    print(item)
+
+if 9 in numbers_set:
+    print('9 exists')
+elif 98 in numbers_set:
+    print('98 exists')
+
+A = {1, 3, 5}
+B = {1, 2, 3, 4, 5}
+print(A & B)
+print(A | B) #AUB
