4865cc26d94569a7b98f407c8f233852dccc19e1
ImAlAmin <alamin.cse97@gmail.com>
2023-05-25 17:22:03 +0600

    3.2 tuples and tuples methods in python

---
+++ phitron_course/Semester 3/Week 01_Introduction to Python/Module 03_Basic Python 02/tuples.py
@@ -0,0 +1,21 @@
+def multiple():
+    return 3, 4
+# print(multiple())
+things = 'pen', 'tripod', 'water bottle', 'charger', 'phone', 'web cam', 'sunglass'
+
+# print(type(things))
+# print(things[0])
+# print(things[-2])
+# print(things[3:6])
+if 'phone' in things:
+    print('exists')
+for item in things:
+    print(item)
+
+# things[0]='wagon'
+# print(things)
+
+item = ('book', 'monitor')
+
+# ignore
+print(len(things))
