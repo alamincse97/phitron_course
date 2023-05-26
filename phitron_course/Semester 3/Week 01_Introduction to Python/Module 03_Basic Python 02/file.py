bc56bb32eb28c04c5cbd17c2db16f42d3f4820a2
ImAlAmin <alamin.cse97@gmail.com>
2023-05-26 07:58:21 +0600

    3.7 Try except finally and file read write in python

---
+++ phitron_course/Semester 3/Week 01_Introduction to Python/Module 03_Basic Python 02/file.py
@@ -0,0 +1,7 @@
+# .csv comma separated value
+# .txt text file
+
+# with open('message.txt', 'w') as file:
+#    file.write('I love you, python!')
+with open('message.txt', 'w') as file:
+    file.write('I love you, python!')
