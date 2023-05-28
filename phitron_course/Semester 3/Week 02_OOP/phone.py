a17c7245793f5615bb78a5a64bbc6df2603c5b11
ImAlAmin <alamin.cse97@gmail.com>
2023-05-28 10:42:23 +0600

    5.1 Introduction to Simple Class

---
+++ phitron_course/Semester 3/Week 02_OOP/phone.py
@@ -0,0 +1,10 @@
+class phone:
+    price = 19000
+    color = 'blue'
+    brand = 'samsung'
+
+my_phone = phone()
+print(my_phone)
+print(my_phone.brand)
+print(my_phone.color)
+print(my_phone.price)
