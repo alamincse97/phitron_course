827b8bcbb505a88a97936547208761be5e0772f2
ImAlAmin <alamin.cse97@gmail.com>
2023-05-28 12:24:54 +0600

    5.3 Constructors and __init__ in python

---
+++ phitron_course/Semester 3/Week 02_OOP/constructors.py
@@ -0,0 +1,22 @@
+class phone:
+    manufactured = 'China'
+
+    def __init__(self, owner, brand, price):
+        self.owner = owner
+        self.brand = brand
+        self.price = price
+
+    def send_sms(self, phone, sms):
+        text = f'sending to: {phone}{sms}'
+        print(text)
+
+my_phone = phone('Kala Chan', 'Oppo', 9800)
+print(my_phone.owner, my_phone.brand, my_phone.price)
+
+her_phone = phone('She amar jaan', 'iphone', 120000)
+print(her_phone.owner, her_phone.brand, her_phone.price)
+# my_phone.send_sms()
+# her_phone.send_sms()
+
+dad_phone = phone('abbu', 'Nokia', 3000)
+print(dad_phone.owner, dad_phone.brand, dad_phone.price)
---
+++ phitron_course/Semester 3/Week 02_OOP/homework.py
@@ -0,0 +1,5 @@
+""""
+1. calculator class to do add, deduct, multiply, divide
+2. pen class. create three object with different instance attribute
+
+"""
