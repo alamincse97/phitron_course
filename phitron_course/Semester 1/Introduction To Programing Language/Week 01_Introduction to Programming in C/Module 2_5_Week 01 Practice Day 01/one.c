91aeaaeada4872bb18ab45577b72f4901b4923b7
alamincse97 <alamin.cse97@gmail.com>
2024-11-11 17:55:49 +0600

    added to update

---
+++ phitron_course/Semester 1/Introduction To Programing Language/Week 01_Introduction to Programming in C/Module 2_5_Week 01 Practice Day 01/one.c
@@ -0,0 +1,17 @@
+/*
+Write a C program to show output like below:
+
+Hi there /\.    I hobe you are doing great and practicing well.     
+Now this is a simple task you need to do.       Just care for the indentation.
+Show 100% of sincerity.
+*/
+
+#include<stdio.h>
+
+int main(){
+    printf("Hi there /\.    I hobe you are doing great and practicing well.");
+    printf("Now this is a simple task you need to do.       Just care for the indentation.");
+    printf("Show 100% of sincerity.");
+
+    return 0;
+}
